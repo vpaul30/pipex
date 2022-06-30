68716a4aa4ca6f2f6dd92cef1ff15a9be38e3c3d
Pavels Vznuzdajevs <pvznuzda@3-G-10.42heilbronn.de>
2022-06-30 16:00:07 +0200

    2

--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 /*   By: pvznuzda <pashavznuzdajev@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 19:47:12 by pvznuzda          #+#    #+#             */
-/*   Updated: 2022/06/29 16:03:58 by pvznuzda         ###   ########.fr       */
+/*   Updated: 2022/06/30 15:59:07 by pvznuzda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include "libft/libft.h"
 
 char	**get_paths(char **envp)
@@ -59,12 +60,38 @@ int	main(int argc, char **argv, char **envp)
 {
 	char	**paths;
 	char	*cmd_path;
-	int		pipefd[2];
+	int		pipefd1[2];
+	int		infile;
+	int		outfile;
+	int		f;
 
-	if (pipe(pipefd) < 0)
+	infile = open(argv[1], O_RDONLY);
+	outfile = open(argv[2], O_WRONLY, O_TRUNC);
+	dup2(infile, 0);
+	if (pipe(pipefd1) < 0)
 		return (0);
-	paths = get_paths(envp);
-	cmd_path = get_cmd_path(paths, "cat");
-	execve(cmd_path, argv, 0);	
+
+	f = fork();
+	if (f == 0)
+	{
+		close(pipefd1[0]);
+		dup2(pipefd1[1], 1);
+		paths = get_paths(envp);
+		cmd_path = get_cmd_path(paths, "cat");
+		execve(cmd_path, NULL, NULL);
+	}
+	f = fork();
+	if (f == 0)
+	{
+		close(pipefd1[1]);
+		char	*args[3];
+		args[0] = "/usr/bin/grep";
+		args[1] = "line";
+		args[2] = NULL;
+		dup2(pipefd1[0], 0);
+		dup2(outfile, 1);
+		execve("/usr/bin/grep", args, 0);
+		
+	}
 	return (0);
 }
